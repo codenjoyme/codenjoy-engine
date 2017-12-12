4bcaf2f881d483d9984fdb4b583870844b31bee9
Oleksandr Baglai <apofig@gmail.com>
2017-12-12 22:58:54 +0200

    Refactoring. Добевлен новый принтер, который позволяет печатать послойно и в опредеелнной вьюшке. Плюс все классы Printer переимещены в отдельный пакет.

--- src/main/java/com/codenjoy/dojo/services/printer/BoardReader.java
+++ src/main/java/com/codenjoy/dojo/services/printer/BoardReader.java
@@ -1,4 +1,4 @@
-package com.codenjoy.dojo.client;
+package com.codenjoy.dojo.services.printer;
 
 /*-
  * #%L
@@ -23,37 +23,14 @@ package com.codenjoy.dojo.client;
  */
 
 
-import com.codenjoy.dojo.services.printer.CharElements;
+import com.codenjoy.dojo.services.Point;
 
 /**
- * Created by indigo on 2016-09-22.
+ * Этот абстракция над доской для Printer
  */
-enum Elements implements CharElements {
+public interface BoardReader {
 
-    ONE('1'), TWO('2'), THREE('3'), FOUR('4'), FIVE('5'), SIX('6'), NONE(' ');
+    int size();
 
-    final char ch;
-
-    Elements(char ch) {
-        this.ch = ch;
-    }
-
-    @Override
-    public char ch() {
-        return ch;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(ch);
-    }
-
-    public static Elements valueOf(char ch) {
-        for (Elements el : Elements.values()) {
-            if (el.ch == ch) {
-                return el;
-            }
-        }
-        throw new IllegalArgumentException("No such element for " + ch);
-    }
+    Iterable<? extends Point> elements();
 }
