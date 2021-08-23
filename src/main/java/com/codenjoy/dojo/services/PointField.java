cc0059c48301d99b059af5d7f303043b616d327a
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-08-23 18:34:16 +0300

    [engine][point-field] Added other useful methods.

--- src/main/java/com/codenjoy/dojo/services/PointField.java
+++ src/main/java/com/codenjoy/dojo/services/PointField.java
@@ -2,10 +2,7 @@ package com.codenjoy.dojo.services;
 
 import com.codenjoy.dojo.services.Point;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PointField {
 
@@ -66,20 +63,29 @@ public class PointField {
         return field[point.getX()][point.getY()];
     }
 
-    public interface Accessor<T> {
+    public interface Accessor<T> extends Iterable<T> {
         <E extends Point> boolean contains(E element);
 
         <E extends Point> void remove(E element);
 
         List<T> all();
 
+        // TODO added stream method
+
         void removeNotIn(List<T> valid);
 
         void add(T element);
+
+        int size();
     }
 
     public <T extends Point> Accessor<T> of(Class<T> filter) {
         return new Accessor<>() {
+            @Override
+            public Iterator<T> iterator() {
+                return all().iterator();
+            }
+
             @Override
             public <E extends Point> boolean contains(E element) {
                 return get(element).contains(filter);
@@ -93,8 +99,8 @@ public class PointField {
             @Override
             public List<T> all() {
                 List<T> result = new LinkedList<>();
-                for (int x = 0; x < size(); x++) {
-                    for (int y = 0; y < size(); y++) {
+                for (int x = 0; x < PointField.this.size(); x++) {
+                    for (int y = 0; y < PointField.this.size(); y++) {
                         T element = field[x][y].get(filter);
                         if (element != null) {
                             result.add(element);
@@ -114,6 +120,11 @@ public class PointField {
             @Override
             public void add(T element) {
                 PointField.this.add(element);
+            }
+
+            @Override
+            public int size() {
+                return all().size();
             }
         };
     }
