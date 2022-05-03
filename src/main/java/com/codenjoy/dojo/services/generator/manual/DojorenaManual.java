37e4be47e8d210dd8503bfe049d47ca78ad405a0
Pavel Reshetylo <pavlo_reshetylo@epam.com>
2022-05-03 15:45:30 +0300

    [manuals] added DojorenaManualGenerator and parts for manual

---
+++ src/main/java/com/codenjoy/dojo/services/generator/manual/DojorenaManual.java
@@ -0,0 +1,64 @@
+package com.codenjoy.dojo.services.generator.manual;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2012 - 2022 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DojorenaManual extends GameManualGenerator {
+
+    private static final String DOJORENA_MANUAL_MD = "dojorena-manual";
+    private static final String GLOBAL_SOURCES = "engine/src/main/resources/manuals/";
+    private static final String GAME_SOURCES = "{$game}/src/main/webapp/resources/{$game}/help/";
+
+    public DojorenaManual(String game, String language, String basePath) {
+        this(game, language, basePath, GLOBAL_SOURCES, GAME_SOURCES);
+    }
+
+    public DojorenaManual(String game, String language, String basePath, String globalSources, String gameSources) {
+        super(game, language, basePath, globalSources, gameSources);
+    }
+
+    @Override
+    protected List<String> getManualParts() {
+        return Arrays.asList("{$global}01-general-header.md",
+                "{$game}{$language}02-dojorena-intro.md",
+                "{$game}{$language}03-general-game-about.md",
+                "{$global}{$language}04-dojorena-how-connect.md",
+                "{$global}{$language}05-general-message-format.md",
+                "{$game}{$language}06-general-field.md",
+                "{$game}elements.md",
+                "{$game}{$language}08-general-what-to-do.md",
+                "{$global}{$language}09-general-ask-sensei.md",
+                "{$game}{$language}10-general-hints.md",
+                "{$game}{$language}11-general-client-and-api.md",
+                "{$game}{$language}12-general-faq.md",
+                "{$global}{$language}13-general-how-to-host.md"
+                );
+    }
+
+    @Override
+    protected String getManualType() {
+        return DOJORENA_MANUAL_MD;
+    }
+}
--- src/main/resources/manuals/en/13-general-how-to-host.md
+++ src/main/resources/manuals/en/13-general-how-to-host.md
@@ -1,4 +1,4 @@
-"## Want to host an event?
+## Want to host an event?
 
 It's an open source game. To implement your version of it,
 to fix bugs and to add any other logic simply
@@ -8,4 +8,4 @@ All instructions are in Readme.md file, you'll know what to do next once you rea
 If you have any questions reach me in [skype alexander.baglay](skype:alexander.baglay)
 or email [apofig@gmail.com](mailto:apofig@gmail.com).
 
-Good luck and may the best win!"
+Good luck and may the best win!
